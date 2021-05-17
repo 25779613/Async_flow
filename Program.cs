b7344975f965fa125e54bbe4597864e04c382add
25779613 <duk.sal01@gmail.com>
2021-05-17 23:12:42 +0200

    making code more legible

--- Program.cs
+++ Program.cs
@@ -9,23 +9,44 @@ namespace Async_Flow
         static async Task Main(string[] args)
         {
             Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<< moving next within Pending Tasks>>>>>>>>>>>>>>>>>");
-            IAsyncEnumerator<int> asyncEnumerator = pendingTask(1, 5).GetAsyncEnumerator();       
-            
-            while(await asyncEnumerator.MoveNextAsync())
-            {
-                Console.WriteLine($"task being run: {(asyncEnumerator.Current)}");
-            }
+            // collection of tasks to be run one after another
+            await runPendingtask();
         
         }
 
+       
         //pending tasks, running through a loop to complete tasks
 
         static async IAsyncEnumerable<int> pendingTask(int start, int iterator)
         {
             for(int i=0; i<iterator;i++)
             {
-                await Task.Delay(2000);
-                yield return start = +i;
+                //await Task.Delay(2000);
+                if(i>2)
+                {
+                    Console.WriteLine($"Chill slow down you only needed 2 tasks not:{i}");
+                }
+
+                await Task.Run(async () =>
+                {
+                    //task1
+                    await Task.Delay(1000);
+                    //task2
+                    await Task.Delay(1000);
+                });
+
+                yield return start =+i;
+            }
+        }
+
+        // method to exe pending task
+        static async Task runPendingtask()
+        {
+            IAsyncEnumerator<int> asyncEnumerator = pendingTask(1, 5).GetAsyncEnumerator();
+
+            while (await asyncEnumerator.MoveNextAsync())
+            {
+                Console.WriteLine($"task being run: {(asyncEnumerator.Current)}");
             }
         }
     }
