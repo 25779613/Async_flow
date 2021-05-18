47bad7f871a6720f922a9e8b5f492f1c132e5bdb
25779613 <duk.sal01@gmail.com>
2021-05-18 21:08:47 +0200

    custom tasks

--- Program.cs
+++ Program.cs
@@ -11,8 +11,10 @@ namespace Async_Flow
             Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<< moving next within Pending Tasks>>>>>>>>>>>>>>>>>");
             // collection of tasks to be run one after another
             await runPendingtask();
-        
+            Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<< Custom Tasks>>>>>>>>>>>>>>>>>");
+            await doJob1();
         }
+
 
        
         //pending tasks, running through a loop to complete tasks
@@ -49,5 +51,29 @@ namespace Async_Flow
                 Console.WriteLine($"task being run: {(asyncEnumerator.Current)}");
             }
         }
+
+        //doing multiple tasks
+        static async Task doJob1()
+        {
+            await Task.Run(async () => {
+                await Task.Delay(1000);
+                Console.WriteLine("Go to mall");
+                await Task.Delay(2000);
+                Console.WriteLine("Buy groceries");
+            }
+
+            );
+        }
+        static async Task doJob2()
+        {
+            await Task.Run(async () => {
+                await Task.Delay(1000);
+                Console.WriteLine("Go to vodacom");
+                await Task.Delay(2000);
+                Console.WriteLine("Buy new phone");
+            }
+            );   
+        }
+
     }
 }
